6d3ca158112a1ddc6d13fe196e2a2553355eaffa
Ogurchick <148265956+ogurchick411@users.noreply.github.com>
2026-07-31 17:31:31 +0200

    add DOM elements and random character generator functions

--- script.js
+++ script.js
@@ -1,4 +1,10 @@
 const resultEl = document.getElementById('result');
+const lengthEl = document.getElementById('length');
+const uppercaseEl = document.getElementById('uppercase');
+const lowercaseEl = document.getElementById('lowercase');
+const numbersEl = document.getElementById('numbers');
+const symbolsEl = document.getElementById('symbols');
+const generateEl = document.getElementById('generate');
 
 const getRandomLower = () => String.fromCharCode(Math.floor(Math.random() * 26) + 97);
 const getRandomUpper = () => String.fromCharCode(Math.floor(Math.random() * 26) + 65);
@@ -8,4 +14,9 @@ const getRandomSymbol = () => {
     return symbols[Math.floor(Math.random() * symbols.length)];
 };
 
-console.log(getRandomLower(), getRandomUpper(), getRandomNumber(), getRandomSymbol());
+const randomFunc = {
+    lower: getRandomLower,
+    upper: getRandomUpper,
+    number: getRandomNumber,
+    symbol: getRandomSymbol
+};
